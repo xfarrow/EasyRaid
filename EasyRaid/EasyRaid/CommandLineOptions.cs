b021f2c985d77a46683600d8ac7a1da305f59540
Alessandro Ferro <49845537+xfarrow@users.noreply.github.com>
2025-10-07 21:58:26 +0200

    Better synopsis

--- EasyRaid/EasyRaid/CommandLineOptions.cs
+++ EasyRaid/EasyRaid/CommandLineOptions.cs
@@ -1,19 +1,92 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 
 namespace EasyRaid
 {
     public class CommandLineOptions
     {
-        [Option('v', "name", Required = false, HelpText = "Print the current version")]
-        public bool Version { get; set; }
+        [Option('n', "new-config", HelpText = "Creates a new configuration file. Requires <SOURCE_PATH> and <DESTINATION_PATH>.", Min = 2, Max = 2)]
+        public IEnumerable<string> NewConfig { get; set; } = new List<string>();
 
-        [Option('h', "help", Required = false, HelpText = "Get help for this program")]
-        public bool Help { get; set; }
+        [Option('c', "config", HelpText = "Loads an existing configuration file.")]
+        public string Config { get; set; } = Configuration.DEFALT_CONFIGURATION_FILE_PATH;
 
-        [Option('n', "new-config", Required = false, HelpText = "Create a new configuration file")]
-        public IEnumerable<string>? NewConfig { get; set; }
+        [Option("verbose", HelpText = "Enables verbose mode.")]
+        public bool Verbose { get; set; }
+    }
 
-        [Option('c', "config", Required = false, HelpText = "Set an existing configuration file")]
-        public string? Config { get; set; }
+    public class Utils
+    {
+        public static CommandLineOptions HandleCommandLineOptions(string[] args)
+        {
+            var parser = new Parser(with =>
+            {
+                with.HelpWriter = null; // Set to Console.Out if you want automatic help text
+            });
+
+            var result = parser.ParseArguments<CommandLineOptions>(args);
+
+            result
+                // The library successfully parsed the command line
+                .WithParsed(o =>
+                {
+                    if (o.NewConfig != null && o.NewConfig.Any())
+                    {
+                        var paths = o.NewConfig.ToArray();
+                        if (paths.Length == 2)
+                        {
+                            string source = paths[0];
+                            string destination = paths[1];
+                            Configuration.CreateConfigurationFile(source, destination);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You must specify both source and destination paths.");
+                            Environment.Exit(1);
+                        }
+                        return;
+                    }
+
+                    if (!string.IsNullOrEmpty(o.Config))
+                    {
+                        if (!File.Exists(o.Config))
+                        {
+                            Console.WriteLine($"Configuration file not found: {o.Config}");
+                            Environment.Exit(1);
+                        }
+                        return;
+                    }
+                })
+
+            // The library did not successfully parse the command line
+            .WithNotParsed(errors =>
+            {
+                if (errors.IsVersion())
+                {
+                    Console.WriteLine("EasyRaid version 0.1");
+                    Environment.Exit(0);
+                }
+
+                else if (errors.IsHelp())
+                {
+                    var helpText = HelpText.AutoBuild(result, h =>
+                    {
+                        h.Heading = "EasyRaid Help";
+                        h.AddPreOptionsLine("EasyRaid is a file-based RAID1-like application.");
+                        return h;
+                    }, e => e);
+                    Console.WriteLine(helpText);
+                    Environment.Exit(0);
+                }
+
+                else
+                {
+                    Console.WriteLine("Command not recognized. Use --help for more info");
+                }
+                Environment.Exit(1);
+            });
+
+            return result.Value;
+        }
     }
 }
--- EasyRaid/EasyRaid/Program.cs
+++ EasyRaid/EasyRaid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using System.Text.Json;
 
 namespace EasyRaid
@@ -7,72 +8,8 @@ namespace EasyRaid
     {
         static int Main(string[] args)
         {
-            string configurationFilePath = Configuration.DEFALT_CONFIGURATION_FILE_PATH;
-
-            Parser.Default.ParseArguments<CommandLineOptions>(args)
-                .WithParsed(o =>
-                {
-                    if (o.Help)
-                    {
-                        Console.WriteLine("EasyRaid is a file based Raid1-like application. WARNING! This is an alpha-state program. Use with caution");
-                        Console.WriteLine("Usage:");
-                        Console.WriteLine("EasyRaid [OPTIONS]\n");
-                        Console.WriteLine("OPTIONS:\n" +
-                            "-v, --version\n" +
-                            "\tPrints the current version and exits immediately.\n\n" +
-                            "-h, --help\n" +
-                            "\tDisplays this help information.\n\n" +
-                            "-n, --new-config <SOURCE_PATH> <DESTINATION_PATH>\n" +
-                            "\tCreates a new configuration file.\n" +
-                            "\tBoth source and destination paths are required\n\n" +
-                            "-c, --config <CONFIG_FILE_PATH>\n" +
-                            "\tLoads an existing configuration file.");
-                        Environment.Exit(0); // Exit immediately
-                    }
-
-                    if (o.Version)
-                    {
-                        Console.WriteLine("EasyRaid version 0.1");
-                        Environment.Exit(0); // Exit immediately
-                    }
-
-                    // Create a new configuration file
-                    if (o.NewConfig != null && o.NewConfig.Any())
-                    {
-                        if (o.NewConfig.Count() == 2)
-                        {
-                            string source = o.NewConfig.ElementAt(0);
-                            string destination = o.NewConfig.ElementAt(1);
-                            Configuration.CreateConfigurationFile(source, destination);
-                        }
-                        else
-                        {
-                            Console.WriteLine("In order to create a new configuration file, you should specify two arguments. Example: EasyRaid <source> <destination>");
-                        }
-
-                        return;
-                    }
-
-                    if (!string.IsNullOrEmpty(o.Config))
-                    {
-                        if (File.Exists(o.Config))
-                        {
-                            Console.WriteLine($"Loaded configuration from {o.Config}");
-                            string content = File.ReadAllText(o.Config);
-                            Console.WriteLine(content);
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Configuration file not found: {o.Config}");
-                        }
-                        return;
-                    }
-
-                    // Default here
-
-                });
-
-            ConfigurationFile? configuration = JsonSerializer.Deserialize<ConfigurationFile>(File.ReadAllText(configurationFilePath));
+            CommandLineOptions options = Utils.HandleCommandLineOptions(args);
+            ConfigurationFile? configuration = JsonSerializer.Deserialize<ConfigurationFile>(File.ReadAllText(options.Config));
             if (configuration == null)
             {
                 Console.WriteLine("Error! No configuration file found. Type easyraid -h for more info");
